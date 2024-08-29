7232b7bdac22437170f52c7a832a73679e6e0365
suraj raghuvanshi <142202030+surajraghuwanshi05@users.noreply.github.com>
2024-08-29 11:28:58 +0530

    Update linear_search.py

--- searching_algo/linear_search.py
+++ searching_algo/linear_search.py
@@ -1,1 +1,12 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # Return the index where the target is found
+    return -1  # Return -1 if the target is not found
+
+# Example usage:
+arr = [10, 15, 30, 70, 80, 60, 20, 90, 40]
+target = 20
+result = linear_search(arr, target)
+print(result)  # Output: 6
 
