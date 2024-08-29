1a10c6317e90cea1a3ca1dd08119bbe8a34eb28e
suraj raghuvanshi <142202030+surajraghuwanshi05@users.noreply.github.com>
2024-08-29 08:29:55 +0530

    Update Selection_sort.py

--- sorting_algo/Selection_sort.py
+++ sorting_algo/Selection_sort.py
@@ -1,1 +1,14 @@
-
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Assume the first unsorted element is the minimum
+        min_index = i
+        # Find the index of the minimum element in the remaining unsorted array
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        # Swap the found minimum element with the first unsorted element
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr
