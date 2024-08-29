c254f4581ecba35deca29417954af725fe44ef8c
suraj raghuvanshi <142202030+surajraghuwanshi05@users.noreply.github.com>
2024-08-29 08:35:38 +0530

    Update Insertion_sort.py

--- sorting_algo/Insertion_sort.py
+++ sorting_algo/Insertion_sort.py
@@ -1,1 +1,15 @@
+def insertion_sort(arr):
+    n = len(arr)  
+
+    # Start from the second element (index 1) to the end of the array
+    for i in range(1, n):
+        # Traverse the sorted portion of the array backwards
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                # If the current element is smaller, swap them
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
+    
+    return arr  
 
