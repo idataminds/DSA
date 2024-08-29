b3b07c9e82d238721953215a09354ff30281f93a
suraj raghuvanshi <142202030+surajraghuwanshi05@users.noreply.github.com>
2024-08-29 08:57:50 +0530

    Update stack.py

--- Stack/stack.py
+++ Stack/stack.py
@@ -1,1 +1,31 @@
+# stack using list
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+
+    def is_empty(self):
+        return len(self.items)==0
+
+
+    def push(self, data):
+        self.items.append(data)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("stack us empty")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+        else:
+            raise IndexError("stack is empty")
+
+    def size(self):
+        return len(self.items)
 
